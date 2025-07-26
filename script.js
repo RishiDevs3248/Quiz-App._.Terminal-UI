30db92a9e1e3aee79c9e88120af12b7da24b84f2
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-07-26 11:31:53 +0530

    progress issue solved

--- script.js
+++ script.js
@@ -1,40 +1,56 @@
 let arr = [
     {
-        questions: "0 ",
+        questions: "What is the use of `let` keyword in JavaScript?",
         options: [
-            "red", "green", "blue", "yellow"
+            "Defines a variable globally",
+            "Defines a constant variable",
+            "Defines a block-scoped variable",
+            "Defines a private function"
         ],
-        ans: "red"
+        ans: "Defines a block-scoped variable"
     },
     {
-        questions: "what is used not",
+        questions: "Which method is used to convert JSON data to a JavaScript object?",
         options: [
-            "red", "green", "blue", "yellow"
+            "JSON.toObject()",
+            "JSON.parse()",
+            "JSON.stringify()",
+            "JSON.convert()"
         ],
-        ans: "red"
+        ans: "JSON.parse()"
     },
     {
-        questions: "1 ",
+        questions: "Which of the following is a JavaScript data type?",
         options: [
-            "red", "green", "blue", "yellow"
+            "float",
+            "number",
+            "decimal",
+            "character"
         ],
-        ans: "red"
+        ans: "number"
     },
     {
-        questions: "2 ",
+        questions: "What will `typeof null` return?",
         options: [
-            "red", "green", "blue", "yellow"
+            "null",
+            "undefined",
+            "object",
+            "boolean"
         ],
-        ans: "red"
+        ans: "object"
     },
     {
-        questions: "3 ",
+        questions: "Which function is used to delay code execution in JavaScript?",
         options: [
-            "red", "green", "blue", "yellow"
+            "delay()",
+            "setInterval()",
+            "setTimeout()",
+            "timeout()"
         ],
-        ans: "red"
-    },
-]
+        ans: "setTimeout()"
+    }
+];
+
 
 
 
@@ -43,20 +59,49 @@ let i = 0;
 
 function handleClicknext() {
     i++;
-    let main = document.querySelector("#bottom_terminal_section")
+      let queno = document.querySelector("#Que_no")
+    let queno2 = document.querySelector("#Que_no2")
+    let total_no = document.querySelector("#total_no")
+    let prog = document.querySelector("#custum_prog")
+    let que = document.querySelector("#Que_text")
+    let opt1 = document.querySelector("#opt_1")
+    let opt2 = document.querySelector("#opt_2")
+    let opt3 = document.querySelector("#opt_3")
+    let opt4 = document.querySelector("#opt_4")
     let div = document.createElement("div");
-    div.innerHTML = `<div class="question">${arr[(i%n)].questions}</div>
-    <div class="options">${arr[(i%n)].questions}</div>`
-    main.innerHTML = ""
-    main.appendChild(div) 
+    que.innerHTML = `${arr[(i%n)].questions}`
+    opt1.innerHTML = `${arr[(i%n)].options[0]}`
+    opt2.innerHTML = `${arr[(i%n)].options[1]}`
+    opt3.innerHTML = `${arr[(i%n)].options[2]}`
+    opt4.innerHTML = `${arr[(i%n)].options[3]}`
+    queno.innerHTML = `${i%n+1}`
+    queno2.innerHTML = `${i%n+1}`
+    prog.innerHTML = `${(( (i % n) + 1 ) / n) * 100}`
+    total_no.innerHTML = `${n}`
+
 }
 
 function handleClickPrev() {
     i--;
-    let main = document.querySelector("#bottom_terminal_section")
+    
+    let queno = document.querySelector("#Que_no")
+    let queno2 = document.querySelector("#Que_no2")
+    let total_no = document.querySelector("#total_no")
+    let prog = document.querySelector("#custum_prog")
+    let que = document.querySelector("#Que_text")
+    let opt1 = document.querySelector("#opt_1")
+    let opt2 = document.querySelector("#opt_2")
+    let opt3 = document.querySelector("#opt_3")
+    let opt4 = document.querySelector("#opt_4")
     let div = document.createElement("div");
-    div.innerHTML = `<div class="question">${arr[(i%n)].questions}</div>
-    <div class="options">${arr[(i%n)].questions}</div>`
-    main.innerHTML = ""
-    main.appendChild(div)
+    que.innerHTML = `${arr[(i%n)].questions}`
+    opt1.innerHTML = `${arr[(i%n)].options[0]}`
+    opt2.innerHTML = `${arr[(i%n)].options[1]}`
+    opt3.innerHTML = `${arr[(i%n)].options[2]}`
+    opt4.innerHTML = `${arr[(i%n)].options[3]}`
+    queno.innerHTML = `${i%n+1}`
+    queno2.innerHTML = `${i%n+1}`
+    prog.innerHTML = `${(( (i % n) + 1 ) / n) * 100}`
+    total_no.innerHTML = `${n}`
+
 }
