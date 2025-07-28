4c8823a698f0f685442a58c003bf365a0a82c1d2
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-07-28 10:16:23 +0530

    Final patch-up

--- script.js
+++ script.js
@@ -48,6 +48,16 @@ let arr = [
             "timeout()"
         ],
         ans: "setTimeout()"
+    },
+    {
+        questions: "Which function is used to delay code execution in JavaScript?",
+        options: [
+            "delay()",
+            "setInterval()",
+            "setTimeout()",
+            "timeout()"
+        ],
+        ans: "setTimeout()"
     }
 ];
 
@@ -59,6 +69,7 @@ let i = 0;
 const options = document.querySelectorAll(".options");
 const Selected_option = document.getElementById("Selected_opt")
 
+
 let queno = document.querySelector("#Que_no")
 let queno2 = document.querySelector("#Que_no2")
 let total_no = document.querySelector("#total_no")
@@ -68,17 +79,46 @@ let opt1 = document.querySelector("#opt_1")
 let opt2 = document.querySelector("#opt_2")
 let opt3 = document.querySelector("#opt_3")
 let opt4 = document.querySelector("#opt_4")
+let x;
+let Curr_que;
+let points = document.getElementById("custum_points")
+
+options.forEach(option => {
+    
+    option.addEventListener("click", () => {
+        options.forEach(opt => opt.classList.remove("selected"));
+        option.classList.add("selected");
+        x = option.dataset.value;
+        switch (x) {
+            case 'A':
+                x = 0;
+                break;
+            case 'B':
+                x = 1;
+                break;
+            case 'C':
+                x = 2;
+                break;
+            case 'D':
+                x = 3;
+                break;
+            default:
+                break
+        }
+        Selected_option.innerText = "--option=" + option.dataset.value;
+    });
+});
+
 
 function handleOptionsque() {
     que.innerHTML = ""
     let txt = arr[(i % n)].questions
-    let speed = 50;
     let j = 0
     function typeWriter() {
         if (j < txt.length) {
             que.innerHTML += txt.charAt(j);
             j++;
-            setTimeout(typeWriter, speed);
+            setTimeout(typeWriter, 50);
         }
     }
     typeWriter()
@@ -89,22 +129,32 @@ function handleOptionsque() {
     opt4.innerHTML = `${arr[(i % n)].options[3]}`
     queno.innerHTML = `${i % n + 1}`
     queno2.innerHTML = `${i % n + 1}`
-    prog.innerHTML = `${(((i % n) + 1) / n) * 100}`
+    prog.innerHTML = `${((((i % n) + 1) / n) * 100).toFixed(2)}`
     total_no.innerHTML = `${n}`
 }
 handleOptionsque()
 
 function handleClicknext() {
+    Curr_que = arr[i%n];
     i++;
 
-
     handleOptionsque()
+    console.log( "IF condition : "+Curr_que.options[x] == Curr_que.ans )
+    console.log(Curr_que)
+    console.log(Curr_que.options[x])
+    console.log(Curr_que.ans)
+    if(Curr_que.options[x] == Curr_que.ans){
+        let prevPoints = Number(points.innerText)
+        points.innerText = prevPoints+=5
+
+    }
     options.forEach(opt => opt.classList.remove("selected"));
     Selected_option.innerText = ""
 
 }
 
 function handleClickPrev() {
+    Curr_que = arr[i%n];
     i--;
     handleOptionsque()
     options.forEach(opt => opt.classList.remove("selected"));
@@ -115,10 +165,3 @@ function handleClickPrev() {
 
 
 
-options.forEach(option => {
-    option.addEventListener("click", () => {
-        options.forEach(opt => opt.classList.remove("selected"));
-        option.classList.add("selected");
-        Selected_option.innerText = "--option=" + option.dataset.value;
-    });
-});
